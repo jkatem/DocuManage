209672be6da5bdae56f2cf3968fecf6cddfc504a
jkatem <jenmiao@gmail.com>
2021-03-21 10:47:38 -0700

    add task item that belongs to the case

--- src/components/matters/Matter.js
+++ src/components/matters/Matter.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { deleteMatter } from '../../redux/actions/mattersActions'
+import { getTasks } from '../../redux/actions/tasksActions'
 import './matter.css'
 
 function Matter(props) {
@@ -12,8 +13,9 @@ function Matter(props) {
 
     return (
            <> 
+           
             <div className="matter">
-                <h6>Case Title: {props.matters.case_title}</h6>
+                <h3><strong>Case Title: {props.matters.case_title}</strong></h3>
                 <h6>Lead Attorney: {props.matters.lead_attorney}</h6>
                 <h6>Associate Attorney: {props.matters.associate_attorney}</h6>
                 <h6>Client: {props.matters.client}</h6>
@@ -24,11 +26,26 @@ function Matter(props) {
                 <h6>Open Date: {props.matters.open_date}</h6>
                 <h6>Closed Date: {props.matters.closed_date}</h6>
                 <button onClick={handleClick}>Delete Matter</button>
-            </div>
+                <br/>
+                <br/>
             
+                <h3><strong>TASKS</strong></h3>
+                {props.matters.tasks.map(task => 
+                    <div>
+                        <h6>{task.task_name}</h6>
+                        <h6>{task.task_description}</h6>
+                    </div>
+                
+                )
+                }
+        {/* {console.log(props.matters.tasks)} */}
+
+            </div>
             </>
     )
 }
-export default connect(null, {deleteMatter})(Matter)
+
+
+export default connect(null, {deleteMatter, getTasks})(Matter)
 
 
